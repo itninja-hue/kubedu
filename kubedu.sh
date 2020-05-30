f7083a4e44ad947154f21d3601cc2ff9c0c0896a
itninja-hue <harb.aladin@gmail.com>
2020-05-30 10:36:31 +0100

    chore: typo

--- kubedu.sh
+++ kubedu.sh
@@ -10,7 +10,7 @@ del(.spec.volumes[] | select (.persistentVolumeClaim.claimName == null)) |
   volumeMounts: .spec.containers[].volumeMounts[]
 } | 
 select(.volumeName == .volumeMounts.name) ' | while IFS= read line; do
-[ -z "$FIRST_LINE" ] && echo -e "NAMESPACE|ClaimeName|USAGE|MOUNTPATH|CAPACITY" 
+[ -z "$FIRST_LINE" ] && echo "NAMESPACE|ClaimeName|USAGE|MOUNTPATH|CAPACITY" 
 echo -n "$line" | jq -c -r '.namespace,"|",.claimName,"|"' | tr -d '\n'; 
 kubectl exec $(echo "$line" | jq -r '.name') -n $(echo "$line" | jq -r '.namespace') -- du -h -d0 $(echo "$line" | jq -r '.volumeMounts.mountPath') | sed -e 's/\s\+/|/g' |tr -d '\n';
 kubectl get pvc $(echo "$line" | jq -r '.claimName') -n $(echo "$line" | jq -r '.namespace')  -o json | jq -r '"|",.status.capacity.storage' | tr -d '\n';
